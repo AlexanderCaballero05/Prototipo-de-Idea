95c72944892b45e098e8c5312deea977ef90a68a
AlexanderCaballero05 <aacaballero@unah.hn>
2022-07-21 00:31:58 -0600

    Menus

--- Proyecto_Prosecar/js/table.js
+++
@@ -1,9 +0,0 @@
-let myName = 'Natalia';
-let myCity = 'Mexico City';
-
-console.log(`My name is ${myName}. My favorite city is ${myCity}.`)
-
-let newVariable = 'Playing around with typeof.';
-console.log(typeof newVariable);
-newVariable = 1;
-console.log(typeof newVariable);
